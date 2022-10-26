fdc1bee67eab195e77b2ed547063b789cbc7294c
DenisBremel <djbremel08@gmail.com>
2022-10-26 17:35:12 -0500

    Primera parte

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+//Crear las funciones
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-Titulo"> CLÍNICA DENTAL UNAS </h1>
+        <p className='App-subTitulo'> <strong>DENTISTAS CALIFICADOS ESPECIALISTAS EN ESTÉTICA DENTAL</strong></p>
+        <p className='App-Texto'> <strong>¿ Deseas realizarte un tratamiento en específico ? Deja que nuestros especialistas te ayuden ¡RESERVA TU CITA!</strong></p>
+        <img src="LogoDent.png" className="App-logo" alt="logo" />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
